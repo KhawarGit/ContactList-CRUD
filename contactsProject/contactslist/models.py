584c55c4a6a39a6d222c47d4b2e9c90534473807
Khawar Khan <ma3533899@gmail.com>
2024-02-09 01:41:25 +0500

    Added Contact model

--- contactsProject/contactslist/models.py
+++ contactsProject/contactslist/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+
+class Contact(models.Model):
+    first_Name = models.CharField(max_length=200)
+    last_Name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    comment = models.TextField()
+    # CharField -> VARCHAR
+
+    def to_string(self):
+        # return "<td>"+self.first_Name+self.last_Name+"</td><td>"+self.email+"</td><td>"+self.comment+"</td>"
+        return "<strong>Name: </strong>"+self.first_Name + self.last_Name+"</br><strong>Email: </strong>"+self.email+"</br><strong>Comment: </strong>"+self.comment+"</br></br>"
+    
