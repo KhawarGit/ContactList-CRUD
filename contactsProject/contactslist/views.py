1e012a2e79638727f1372b17bd81b3528f976195
Khawar Khan <ma3533899@gmail.com>
2024-02-09 01:45:15 +0500

    Importing necessary libraries in the views.py of contactslist app

--- contactsProject/contactslist/views.py
+++ contactsProject/contactslist/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, reverse
+from .models import Contact
 # Create your views here.
+
+from django.http import HttpResponse
+from django.template import loader
