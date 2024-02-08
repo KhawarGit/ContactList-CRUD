1a2c2088e9b6416f1c5203357513fac5b61b0165
Khawar Khan <ma3533899@gmail.com>
2024-02-09 02:01:15 +0500

    Added view for rendering edit form

--- contactsProject/contactslist/views.py
+++ contactsProject/contactslist/views.py
@@ -26,3 +26,23 @@ def delete(request):
     contact_to_delete.delete()
 
     return redirect(reverse('display-contacts'))
+
+def edit_form(request):
+    template = loader.get_template('edit-contact.html')
+
+    # id = request.GET.get('id')
+    # fn = request.GET.get('fn')
+    # ln = request.GET.get('ln')
+    # email = request.GET.get('email')
+    # comment = request.GET.get('comment')
+    context = {
+        'contact': {
+            'id': request.GET.get('id'),
+            'first_Name': request.GET.get('fn'),
+            'last_Name': request.GET.get('ln'),
+            'email': request.GET.get('email'),
+            'comment': request.GET.get('comment')
+        }
+    }
+    # print(id, fn, ln, email, comment)
+    return HttpResponse(template.render(context, request))
